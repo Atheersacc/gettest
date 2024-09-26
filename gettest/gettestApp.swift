aa55b6d62c0069a7e0d8e47e75ab1872bc60e063
Atheer <Atheertalmutairi@gmail.com>
2024-09-26 17:06:35 +0300

    Initial Commit

---
+++ gettest/gettestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gettestApp.swift
+//  gettest
+//
+//  Created by Atheer on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct gettestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
